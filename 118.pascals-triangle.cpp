ecf19a1b027e9a6c5930a1a6dc5136d9bcf6457f
batmanShu <shengjieli897@163.com>
2019-08-25 11:05:13 +0800

    Update 118.pascals-triangle.cpp

--- 118.pascals-triangle.cpp
+++ 118.pascals-triangle.cpp
@@ -6,7 +6,44 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> result;
+        if(numRows==0)
+        {
+            return result;
+        }
+        if(numRows==1)
+        {
+            vector<int> temp;
+            temp.push_back(1);
+            result.push_back(temp);
+        }
+        else
+        {
+            int lastnumRows;
+            lastnumRows=numRows-1;
+            result=generate(lastnumRows);
+            vector<int> currentVector;
+            vector<int> lastVector;
+            lastVector=result[lastnumRows-1];
+            for(int i=0;i<numRows;i++)
+            {
+                if(i==0) 
+                {
+                    currentVector.push_back(1);
+                }
+                else if(i==numRows-1)
+                {
+                    currentVector.push_back(1);
+                }
+                else
+                {
+                    currentVector.push_back(lastVector[i-1]+lastVector[i]);
+                }
+            }
+            result.push_back(currentVector);
+        }
         
+        return result;
     }
 };
 
