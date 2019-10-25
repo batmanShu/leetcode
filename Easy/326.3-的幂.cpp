4bb532305feb7cb45443bd12e27bee916d7391f4
batmanShu <shengjieli897@163.com>
2019-10-25 10:53:46 +0800

    Create 326.3-的幂.cpp

---
+++ Easy/326.3-的幂.cpp
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=326 lang=cpp
+ *
+ * [326] 3的幂
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if(n==0) return false;
+        if(n==1) return true;
+        if(n%3==0)
+        {
+            n=n/3;
+            return isPowerOfThree(n);
+        }
+        else
+        {
+            return false;
+        }
+        
+    }
+};
+// @lc code=end
+
