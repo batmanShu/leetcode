e0a0f37fc03cc6e3d47fc2eff75d71dba9fe141e
batmanShu <shengjieli897@163.com>
2020-03-17 12:19:01 +0800

    Create 面试题50. 第一个只出现一次的字符.cpp

---
+++ 剑指offer/面试题50. 第一个只出现一次的字符.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    char firstUniqChar(string s) {
+        vector<int> c(26,0);
+        for(int i=0;i<s.size();i++)
+        {
+            c[s[i]-'a']++;
+        }
+        for(int i=0;i<s.size();i++)
+        {
+            if(c[s[i]-'a']==1)
+                return s[i];
+        }
+        return ' ';
+    }
+};
