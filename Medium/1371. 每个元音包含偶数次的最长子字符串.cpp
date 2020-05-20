686b44d89967116046651c2a2d4db8056088e9b8
batmanShu <shengjieli897@163.com>
2020-05-20 21:47:20 +0800

    Create 1371. 每个元音包含偶数次的最长子字符串.cpp

---
+++ Medium/1371. 每个元音包含偶数次的最长子字符串.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int findTheLongestSubstring(string s) {
+        vector<int> pos(1<<5,-1);
+        pos[0]=0;
+        int ans=0,status=0;
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]=='a')
+            {
+                status^=1<<0;
+            }
+            else if(s[i]=='e')
+            {
+                status^=1<<1;
+            }
+            else if(s[i]=='i')
+            {
+                status^=1<<2;
+            }
+            else if(s[i]=='o')
+            {
+                status^=1<<3;
+            }
+            else if(s[i]=='u')
+            {
+                status^=1<<4;
+            }
+            if(~pos[status])
+            {
+                ans=max(ans,i+1-pos[status]);
+            }
+            else
+                pos[status]=i+1;
+        }
+        return ans;
+    }
+};
